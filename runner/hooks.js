6d29dd7d4b8d70fbaae971537b69c237e477ba60
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2022-02-03 17:15:28 +0530

    fix

--- runner/hooks.js
+++ runner/hooks.js
@@ -4,9 +4,10 @@ const { chromium } = require('playwright');
 const fs = require('fs')
 var path = require('path');
 require('dotenv').config();
+const CONFIG = require('../config.json')
 
 //BSTACK
-let bStack = process.env.BSTACK
+let bStack = CONFIG.run_on_BS_cloud
 console.log("BSTACK IS " + bStack);
 const options = {
     headless: false,
@@ -17,20 +18,20 @@ const cp = require('child_process');
 const clientPlaywrightVersion = cp.execSync('npx playwright --version').toString().trim().split(' ')[1];
 
 const caps = {
-    'browser': 'chrome',  // allowed browsers are `chrome`, `edge`, `playwright-chromium`, `playwright-firefox` and `playwright-webkit`
-    'os': 'osx',
-    'os_version': 'catalina',
-    'name': 'My first playwright test',
-    'build': 'playwright-build-1',
-    'browserstack.username': process.env.BROWSERSTACK_USERNAME,
-    'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY,
+    'browser': CONFIG.browser,  // allowed browsers are `chrome`, `edge`, `playwright-chromium`, `playwright-firefox` and `playwright-webkit`
+    'os': CONFIG.os,
+    'os_version': CONFIG.os_version,
+    'name': CONFIG.name,
+    'build': CONFIG.build,
+    'browserstack.username': CONFIG.user,
+    'browserstack.accessKey': CONFIG.accessKey,
     'client.playwrightVersion': clientPlaywrightVersion  // Playwright version being used on your local project needs to be passed in this capability for BrowserStack to be able to map request and responses correctly
 };
 
 
 // Create a global browser for the test session.
 BeforeAll(async () => {
-    if (bStack == "true") {
+    if (bStack) {
         console.log("RUNNING TEST ON BROWSERSTACK");
         global.browser = await chromium.connect({
             wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(caps))}`,
