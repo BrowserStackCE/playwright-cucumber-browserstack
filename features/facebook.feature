5e4fd7e025736f914a8e371653b7d14c08a222d1
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2022-02-03 12:49:11 +0530

    fix

--- features/facebook.feature
+++ features/facebook.feature
@@ -3,4 +3,4 @@ Feature: Facebook login validation
   Scenario: Should throw error on incorrect credentials
     Given I am on facebook login page
     When I enter wrong credentials and login
-   
+    Then I should see the error 
--- features/support/googleLoginSteps.js
+++ features/support/googleLoginSteps.js
@@ -19,29 +19,35 @@ Given('I am on facebook login page', async () => {
 
 
 When('I enter wrong credentials and login', async () => {
-    await page.fill('#email', 'some.wrong@gmal.com');
+    await page.fill('#email', 'dksjkdjk');
     await page.fill('#pass', 'some-wrong-password');
-
+    await page.locator('xpath=//button').click();
 
 })
-
 
 Then('I submit', async () => {
     const element1 = await page.$('[aria-label="Search"]');
     await element1.press('Enter');
 });
 
+Then('I should see the error', async () => {
+    try {
+        const text = await page.innerText('._9ay7');
+        expect(text).to.equal('The email or mobile number you entered isn’t connected to an account. Find your account and log in.');
+        await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { status: 'passed', reason: 'Title matched' } })}`);
+    } catch {
+        await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { status: 'failed', reason: 'Title did not match' } })}`);
+
+    }
+})
+
 Then('I should see title BrowserStack', async () => {
     const title = await page.title('');
     console.log(title);
     try {
         expect(title).to.equal("BrowserStack - Google Search", 'Expected page title is incorrect!');
-
-
         // following line of code is responsible for marking the status of the test on BrowserStack as 'passed'. You can use this code in your after hook after each test
         await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { status: 'passed', reason: 'Title matched' } })}`);
-
-
     } catch {
         await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { status: 'failed', reason: 'Title did not match' } })}`);
 
